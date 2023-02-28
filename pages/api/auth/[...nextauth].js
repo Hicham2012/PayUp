f6e76fc515499883ab38646d532b443aa20bc0ba
Hicham-Zaadla <99765449+Hicham2012@users.noreply.github.com>
2023-02-28 13:29:45 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,9 @@ import GoogleProvider from "next-auth/providers/google"
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
+            clientId: {GOOGLE_ID},
+            clientSecret: {GOOGLE_SECRET}
         })
     ],
-    secret: process.env.JWT_SECRET
+    secret: {JWT_SECRET}
 })
